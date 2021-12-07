59a2227ba03048d84ea94b10f4160de56c92670c
psurya1 <suryaswaminathan1999@gmail.com>
2021-12-07 19:00:22 +0530

    shared memory

---
+++ day11/sharedmem/p1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>     
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <semaphore.h>
+
+int *count;
+int num;
+sem_t s1;
+
+
+int main(int argc,char const *srgv[])
+{ 
+
+    int shmd;
+    sem_init(&s1,1,1);
+    shmd=shm_open("/cdac",O_RDWR | O_CREAT,S_IRUSR | S_IWUSR);
+    ftruncate(shmd,sizeof(int));
+    count=(int *)mmap(NULL,sizeof(int),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0);
+    while(1)
+    {   
+        sem_wait(&s1);
+        scanf("%d",&num);
+        *count=num;
+        sem_post(&s1);
+    }
+    sem_destroy(&s1);
+    return 0;
+}
---
+++ day11/sharedmem/p2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>     
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <semaphore.h>
+
+int *count;
+int num;
+sem_t s2;
+
+
+int main(int argc,char const *srgv[])
+{ 
+
+    int shmd;
+    sem_init(&s2,1,1);
+    shmd=shm_open("/cdac",O_RDWR | O_CREAT,S_IRUSR | S_IWUSR);
+    ftruncate(shmd,sizeof(int));
+    count=(int *)mmap(NULL,sizeof(int),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0);
+    while(1)
+    {   
+        sem_wait(&s2);
+        printf("COUNT: %d",*count);
+        sem_post(&s2);
+    }
+    sem_destroy(&s2);
+    return 0;
+}
